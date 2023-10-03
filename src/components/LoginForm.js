209226418dbc2d9558d8c149b68f27e27fc5c459
Jeronimo Arias <jeronimoarias1212@gmail.com>
2023-10-03 16:37:09 -0500

    Delete and create, better response

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,29 @@ import "../templates/LoginForm.css";
 export default function LoginForm() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
+  const [response, setResponse] = useState("");
 
   const handleClick = (e) => {
     e.preventDefault();
-    const student = { name, address };
-    console.log(student);
-    fetch("http://localhost:8080/student/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(student),
-    }).then(() => {
-      console.log("New Student has been Added");
-    });
+    if (name !== "" && address !== "") {
+      const student = { name, address };
+      console.log(student);
+      fetch("http://localhost:8080/student/add", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(student),
+      }).then(() => {
+        alert("New Student has been Added");
+        window.location.reload(false);
+      });
+    }else{
+      setResponse("Completa los campos")
+    }
   };
   return (
-    <form>
+    <form onSubmit={handleClick}>
       <h1 className="form-title">Create Student</h1>
+      <h2 className="form-response">{response}</h2>
       <div className="form-input">
         <input
           type="text"
@@ -40,9 +47,7 @@ export default function LoginForm() {
         />
         <label htmlFor="address">Address:</label>
       </div>
-      <button className="formBtn" onClick={handleClick}>
-        Submit
-      </button>
+      <button className="formBtn">Submit</button>
     </form>
   );
 }
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -4,6 +4,19 @@ import "../templates/MainContent.css";
 export default function MainContent() {
   const [students, setStudents] = useState([]);
 
+  function handleClick(id){
+    const student = { id };
+    console.log(student);
+    fetch("http://localhost:8080/student/delete", {
+      method: "DELETE",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(student),
+    }).then(() => {
+      alert("Student has been Deleted");
+      window.location.reload(false);
+    });
+  };
+
   useEffect(() => {
     fetch("http://localhost:8080/student/getAll")
       .then((res) => res.json())
@@ -16,9 +29,17 @@ export default function MainContent() {
       <div className="cards">
         {students.map((student) => (
           <div className="student-card">
-            <h1>ID: {student.id}</h1>
             <h2>Name: {student.name}</h2>
             <h3>Address: {student.address}</h3>
+            <button
+              className="delete-btn"
+              onClick={(e) => {
+                handleClick(e.target.value);
+              }}
+              value={student.id}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
